104be2b6c2e632a134adb346c9fa9c5ec3e51840
Mykyta-Ushakow <nilitosushak@gmail.com>
2023-10-11 15:47:37 +0300

    After submit SlimSelect fixes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
 import Notiflix from 'notiflix';
 
@@ -12,24 +13,61 @@ const refs = {
 
 const { breedSelect, loaderEl, catInfoEl } = refs;
 
+const events = {
+    afterChange: (newVal) => {
+
+    loaderEl.classList.remove("hidden");
+    catInfoEl.classList.add("hidden");
+
+
+    fetchCatByBreed(newVal[0].value)
+        .then(resp => {
+            catInfoEl.innerHTML = displayInfo(resp.data[0]);
+            catInfoEl.classList.remove("hidden");
+        })
+        .catch(err => {
+            loaderEl.classList.add("hidden");
+            catInfoEl.classList.add("hidden");
+
+            Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!', {
+                timeout: 60000,
+                fontSize: '28px',
+                width: "450px"
+            });
+        })
+        .finally(() => {
+            loaderEl.classList.add("hidden");
+        });
+    }
+} 
+
 addEventListener("load", onLoad);
 
 function onLoad() {   
 
     fetchBreeds()
         .then(resp => {
+            // breedSelect.insertAdjacentHTML("afterbegin",
+            // `
+            // <option value="" disabled selected hidden>
+            // 🐱Choose Your Cat🐱
+            // </option>
+            // `);
             breedSelect.insertAdjacentHTML("afterbegin",
             `
-            <option value="" disabled selected hidden>
-            🐱Choose Your Cat🐱
-            </option>
+            <option data-placeholder="true"></option>
             `);
             breedSelect.insertAdjacentHTML("beforeend", fillOptions(resp.data));
             breedSelect.classList.remove("hidden");
 
-            // new SlimSelect({
-            // select: document.querySelector('.breed-select')
-            // });
+            new SlimSelect({
+                select: '.breed-select',
+                events,
+                settings: {
+                    placeholder: true,
+                    placeholderText: '🐱Choose Your Cat🐱',
+                }
+            });
         })
         .catch(err => {
             loaderEl.classList.add("hidden");
@@ -53,34 +91,6 @@ function fillOptions(cats) {
         .join("");
 };
 
-breedSelect.addEventListener("input", onInput);
-
-function onInput({currentTarget }) {
-
-    loaderEl.classList.remove("hidden");
-    catInfoEl.classList.add("hidden");
-
-
-    fetchCatByBreed(currentTarget.value)
-        .then(resp => {
-            catInfoEl.innerHTML = displayInfo(resp.data[0]);
-            catInfoEl.classList.remove("hidden");
-        })
-        .catch(err => {
-            loaderEl.classList.add("hidden");
-            catInfoEl.classList.add("hidden");
-
-            Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!', {
-                timeout: 60000,
-                fontSize: '28px',
-                width: "450px"
-            });
-        })
-        .finally(() => {
-            loaderEl.classList.add("hidden");
-        });
-}
-
 function displayInfo(cat) {
     
     return `
@@ -92,3 +102,31 @@ function displayInfo(cat) {
     </div>
     `
 };
+
+// breedSelect.addEventListener("input", onInput);
+
+// function onInput({currentTarget }) {
+
+//     loaderEl.classList.remove("hidden");
+//     catInfoEl.classList.add("hidden");
+
+
+//     fetchCatByBreed(currentTarget.value)
+//         .then(resp => {
+//             catInfoEl.innerHTML = displayInfo(resp.data[0]);
+//             catInfoEl.classList.remove("hidden");
+//         })
+//         .catch(err => {
+//             loaderEl.classList.add("hidden");
+//             catInfoEl.classList.add("hidden");
+
+//             Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!', {
+//                 timeout: 60000,
+//                 fontSize: '28px',
+//                 width: "450px"
+//             });
+//         })
+//         .finally(() => {
+//             loaderEl.classList.add("hidden");
+//         });
+// }
